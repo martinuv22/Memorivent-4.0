2cc4958bbafb6bec9979c87448f5fb67741e5361
Martin Utrera <utrera.martin90@gmail.com>
2023-05-02 20:19:43 -0300

    Avance entrega3

--- native_server/script.js
+++ native_server/script.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
-
+//const fs = require('fs')
+import fs from 'fs'
 class ProductManager {
     constructor(path) {
         this.products = []     //para guardar en la memoria todos los productos
@@ -60,36 +60,54 @@ class ProductManager {
         }
     }
     getProducts() {
-        console.log(this.products)
-        return this.products
-    }
-    getProductById(id) {
-        try{
-        let productFound = this.products.find(each=>each.id===id)
-        if(!productFound){
-            console.log('Not found')
-    
-                
-        }else{
-                console.log(productFound)
-           
-                return productFound
+        try {
+        if (this.products.length === 0) {
+            console.log('Not found');
         }
-    }catch(error) {
-        console.log(error)
-        return 'updateProduct: error'
+        console.log(this.products);
+        return this.products;
+        } catch (error) {
+        console.log(error);
+        return 'getProduct: Error';
+        }
     }
-}
-    async upDateProduct(id,data) {
+    read_products() {
+        return this.products;
+    }
+
+    read_product(id) {
+        let productFound = this.products.find((each) => each.id === id);
+        //console.log(productFound)
+        return productFound;
+    }    
+ getProductById(pid) {
+        let buscar = this.read_product(pid);
+        try {
+        if (buscar) {
+            console.log(buscar);
+            return buscar;
+        }
+        console.log('not found');
+        return 'Not found'
+        } catch (error) {
+        console.log(error);
+        return 'getProductById: Error';
+        }
+    }
+        async upDateProduct(id,data) {
         //data es el objeto con las propiedades que necesito modificar del producto
         try {
             //busco el producto
-            let productFound = this.getProductById(id)
+            let productFound = this.read_product(id);
             //itero para modificar la propiedad correspondiente
+            if(productFound){
             for (let prop in data) {
                 //console.log(prop)
                 productFound[prop] = data[prop]
             }
+        }else{ console.log('Not found')
+        return 'Not found'
+        }
             //convierto a texto plano el array
             let data_json = JSON.stringify(this.products,null,2)
             //sobre-escribo el archivo
@@ -103,17 +121,21 @@ class ProductManager {
     }
     async deleteProduct(id) {
         try {
+            let productFound = this.read_product(id);
+            if(productFound){
             //saco el producto
             this.products = this.products.filter(each=>each.id!==id)
             //console.log( this.products)
-           
-           
             //convierto a texto plano el array
             let data_json = JSON.stringify(this.products,null,2)
             //sobre-escribo el archivo
             await fs.promises.writeFile(this.path,data_json)
             console.log('deleteProduct: done'+id)
             return 'deleteProduct: done'+id
+        }else{
+            console.log('Not found')
+            return 'Not found'
+        }
              
         } catch(error) {
             console.log(error)
@@ -121,20 +143,23 @@ class ProductManager {
         }
     }
 }
+
+let manager = new ProductManager('./data/products.json')
 async function up() {
-    let product = new ProductManager('./data/products.json')
-    await product.addProduct({title:'pendrive1',description:'64GB marca Sandisk',price:'$5.000',thumbnail:"imagen"})
-    await product.addProduct({title:'pendrive2',description:'120GB marca Sandisk',price:'$10.000',thumbnail:"imagen",stock:100})
-    await product.addProduct({title:'pendrive3',description:'500GB marca Sandisk',price:'$20.000',thumbnail:"imagen",stock:150})
-    await product.addProduct({title:'pendrive4',description:'64GB marca Kingtong',price:'$8.000',thumbnail:"imagen",stock:50})
-    await product.addProduct({title:'pendrive5',description:'120GB marca Kingtong',price:'$12.000',thumbnail:"imagen",stock:50})
-    await product.addProduct({title:'pendrive6',description:'500GB marca Kingtong',price:'24.000',thumbnail:"imagen",stock:70})
-    await product.addProduct({title:'Tarjeta de Memoria1',description:'64GB marca Kingtong',price:'$8.000',thumbnail:"imagen",stock:60})
-    await product.addProduct({title:'Tarjeta de Memoria2',description:'128GB marca Kingtong',price:'$12.000',thumbnail:"imagen",stock:90})
-    await product.addProduct({title:'Disco Duro Extraible1',description:'1TB',price:'$50.000',thumbnail:"imagen",stock:10})
-    await product.addProduct({title:'Disco Duro Extraible2',description:'2TB',price:'$80.000',thumbnail:"imagen",stock:8})
-    await product. getProductById(9)
-    await product.deleteProduct(10)
-    await product.getProducts()
+    await manager.addProduct({title:'pendrive1',description:'64GB marca Sandisk',price:'$5.000',thumbnail:"imagen"})
+    await manager.addProduct({title:'pendrive2',description:'120GB marca Sandisk',price:'$10.000',thumbnail:"imagen",stock:100})
+    await manager.addProduct({title:'pendrive3',description:'500GB marca Sandisk',price:'$20.000',thumbnail:"imagen",stock:150})
+    await manager.addProduct({title:'pendrive4',description:'64GB marca Kingtong',price:'$8.000',thumbnail:"imagen",stock:50})
+    await manager.addProduct({title:'pendrive5',description:'120GB marca Kingtong',price:'$12.000',thumbnail:"imagen",stock:50})
+    await manager.addProduct({title:'pendrive6',description:'500GB marca Kingtong',price:'24.000',thumbnail:"imagen",stock:70})
+    await manager.addProduct({title:'Tarjeta de Memoria1',description:'64GB marca Kingtong',price:'$8.000',thumbnail:"imagen",stock:60})
+    await manager.addProduct({title:'Tarjeta de Memoria2',description:'128GB marca Kingtong',price:'$12.000',thumbnail:"imagen",stock:90})
+    await manager.addProduct({title:'Disco Duro Extraible1',description:'1TB',price:'$50.000',thumbnail:"imagen",stock:10})
+    await manager.addProduct({title:'Disco Duro Extraible2',description:'2TB',price:'$80.000',thumbnail:"imagen",stock:8})
+    await manager. getProductById(9)
+    await manager.deleteProduct(10)
+    await manager.getProducts()
 }
-up()
+//up()
+
+export default manager
---
+++ native_server/server.js
@@ -0,0 +1,177 @@
+import express from 'express' // importo express
+import manager from './script.js'
+import carts from './carts.js'  
+let server = express()
+
+//defino servidor de express
+
+let PORT = 8080                   //defino el puerto donde se va a levantar el servidor
+let ready = () => console.log('server ready on port: '+PORT)   //defino la callback que se va a ejecutar cuando se levante el servidor
+
+server.listen(PORT,ready)
+server.use(express.json())
+server.use(express.urlencoded({extended:true}))
+
+
+let index_route = '/'
+let index_function = (req,res)=> {
+    let quantity_prod = manager.read_products().length
+    let quantity_cart = carts.read_Carts().length
+    console.log(quantity_prod,quantity_cart)
+    return res.send(`<strong>Welcome to the index page.</strong><br>There are ${quantity_prod} productos and ${quantity_cart} carts.`)
+}
+
+server.get(index_route,index_function)
+
+let api_route = '/api'
+let api_function = (req,res)=> {
+    let quantity_prod = manager.read_products().length
+    let quantity_cart = carts.read_Carts().length
+    console.log(quantity_prod,quantity_cart)
+    return res.send(`<strong>Welcome to the api page.</strong><br>There are ${quantity_prod} productos and ${quantity_cart} carts.`)
+}
+
+server.get(api_route,api_function)
+
+//Products
+
+let one_route = '/api/products/:pid'
+let one_function = (request,response)=>{
+    let parametros = request.params
+    let pid = Number(parametros.pid) 
+    // console.log(id)
+    // console.log(typeof id)
+    let one = manager.read_product(pid)
+    console.log(one)
+    if (one) {
+        return response.send({
+            success: true,
+            product: one
+        })
+    } else {
+        return response.send({
+            success: false,
+            product: 'not found'
+        })
+    }
+    
+}
+server.get(one_route,one_function)
+
+let query_route = '/api/products'
+let query_function = (req,res) => {
+    try {
+        let limit = req.query.limit
+        let products = manager.read_products().slice(0,limit)
+        return res.send({
+            success: true,
+            products
+        })
+    }catch (error) {
+        console.log(error)
+        return res.json({
+            status: 400,
+            message: 'error'
+        })
+    }
+}
+
+server.get(query_route,query_function)
+
+server.post(
+    '/api/products',
+    (req,res)=>{
+        try {
+            let title = req.body.title ?? null
+            let description = req.body.description ?? null
+            let price = req.body.price ?? null
+            let thumbnail = req.body.thumbnail ?? null
+            let stock = req.body.stock ?? null
+            if (title&&description&&price&&thumbnail&&stock){
+                manager.addProduct({ title, description, price, thumbnail, stock })
+                return res.json({
+                    status: 201,
+                    message: 'created'
+                })
+            } else {
+                return res.json({
+                    status: 400,
+                    message:  'check data'
+                })
+            }
+        } catch (error) {
+            console.log(error)
+            return res.json({
+                status: 500,
+                message: 'error'
+            })
+        }
+    }
+)
+
+server.put(
+    '/api/products/:uid',
+    (req,res)=> {
+        try {
+            if(req.body&&req.params.uid){
+                let id = Number(req.params.uid)
+                let data = req.body
+                manager.updateProduct(id, data)
+                return res.json({
+                    status: 200,
+                    messahe: 'Product updated'
+                })
+            } else {
+                return res.json({
+                    status: 400,
+                    message: 'check data'
+                })
+            }
+        } catch (error) {
+            return 'Error'
+        }
+    }
+)
+
+// Carts
+
+let carts_route = '/api/carts/:cid'
+let carts_function = (request,response)=>{
+    let parametros = request.params
+    let cid = Number(parametros.cid) 
+    let cart = carts.read_Cart(cid)
+    console.log(cart)
+    if (cart) {
+        return response.send({
+            success: true,
+            product: cart
+        })
+    } else {
+        return response.send({
+            success: false,
+            product: 'not found'
+        })
+    }
+    
+}
+server.get(carts_route,carts_function)
+
+let carts_query_route = '/api/carts'
+let carts_query_function = (req,res) => {
+    try {
+        let limit = req.query.limit ?? 10
+        let carts = carts.read_Carts().slice(0,limit)
+        return res.send({
+            success: true,
+            carts
+        })
+    }catch (error) {
+        console.log(error)
+        return res.json({
+            status: 400,
+            message: 'error'
+        })
+    }
+}
+
+server.get(carts_query_route,carts_query_function)
